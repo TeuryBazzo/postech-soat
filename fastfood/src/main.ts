8f77c0f920cf51b41b275eecc74b0ba31f58ebe0
Téury Simões Bazzo <teurybazzo@gmail.com>
2023-06-23 19:43:48 -0300

    adicionado swagger

--- fastfood/src/main.ts
+++ fastfood/src/main.ts
@@ -2,14 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { OrderModule } from './order/order.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrderModule);
   
   //Pipes
   app.useGlobalPipes(new ValidationPipe());
-  
+
   useContainer(app.select(OrderModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('fasfood soat1')
+    .setDescription('API de pedidos')
+    .setVersion('1.0')
+    .addTag('fastfood')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3000);
 }
