4b4e49580056154a55e3d0720d57efdb69db5a86
Téury Simões Bazzo <teurybazzo@gmail.com>
2023-10-23 19:44:21 -0300

    adicionada funcao http q busca por cpf

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -9,10 +9,12 @@ import { CreateClientuseCase } from "./useCases/createClient.useCase";
 import { GetClientByCpfuseCase } from "./useCases/getClientByCpf.useCase";
 import { ReponseHttpHelper } from "src/presentation/helpers/excption.http.helper";
 import { IsUniqueCpfuseCase } from "./useCases/isUniqueCpf.useCase";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Client, Order])
+        TypeOrmModule.forFeature([Client, Order]),
+        HttpModule
     ],
     controllers: [ClientController],
     providers: [
